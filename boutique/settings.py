f4969d862ae87c492105759a765c6aa6d6bd5920
andreas2104 <andrianiainaandreas99@gmail.com>
2025-05-22 22:58:06 +0300

    feautre on product

--- boutique/settings.py
+++ boutique/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'magasin',
+   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'magasin',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'boutique.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'ecommerce_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : 'localhost'
     }
 }
 
--- magasin/views.py
+++ magasin/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
 
-# Create your views here.
 from .models import Produit
+
+class ProduitForm(forms.ModelForm):
+  class Meta:
+    model = Produit
+    fields = ['nom', 'prix', 'description']
 
 def liste_produits(request):
   produits = Produit.objects.all()
   return render(request, 'magasin/liste.html', {'produits': produits})
+
+
+def ajouter_Produit(request):
+  if request.method == 'POST':
+    form = ProduitForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('liste_produits')
+  else:
+      form = ProduitForm()
+  return render(request, 'magasin/ajouter.html', {'form': form})
+    
